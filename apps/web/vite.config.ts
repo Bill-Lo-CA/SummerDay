bcbd68affb2f69179619b3e82b1fc0d822d2ba43
lagane <laganeca@gmail.com>
2026-07-13 18:19:03 -0400

    fix: proxy media assets in vite dev server

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
-  server: { proxy: { '/api': 'http://localhost:8000' } }
+  server: { proxy: { '/api': 'http://localhost:8000', '/media': 'http://localhost:8000' } }
 });
-
